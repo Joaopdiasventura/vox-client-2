c6df236ee826c2deb965ff27624772be9d83a8ab
João Pedro Vitorio <joaopedronm53@gmail.com>
2025-09-17 21:27:19 -0300

    chore:adição do arquivo eslint

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const angular = require("@angular-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.recommended,
+    ],
+    // processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        { accessibility: "explicit" },
+      ],
+    },
+  },
+];
